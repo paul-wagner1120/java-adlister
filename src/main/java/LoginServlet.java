cfbaa97c391e79e265c9faff907d328e02eb8b06
Paul Wagner <p.wagner1120@icloud.com>
2023-02-08 09:05:45 -0600

    adding sessions and cookies exercises

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,20 +3,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.invalidate();
         request.getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession();
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean validAttempt = username.equals("admin") && password.equals("password");
 
         if (validAttempt) {
+            session.setAttribute("user", true);
             response.sendRedirect("/profile");
         } else {
             response.sendRedirect("/login");
